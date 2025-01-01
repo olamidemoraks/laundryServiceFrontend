5147a245d037b95cc920b518ac86715c1ffcb23e
Morakinyo David <83555577+olamidemoraks@users.noreply.github.com>
2025-01-01 11:55:36 +0100

    Update overview.ts

--- src/app/api/overview.ts
+++ src/app/api/overview.ts
@@ -1,4 +1,4 @@
-const baseUrl = "https://laundryservicebackend.onrender.com/api/v1/overview";
+const baseUrl = "https://excel-laundary.onrender.com/api/v1/overview";
 const info = JSON.parse(localStorage.getItem("_profile") as string) ?? {
   token: "",
 };
